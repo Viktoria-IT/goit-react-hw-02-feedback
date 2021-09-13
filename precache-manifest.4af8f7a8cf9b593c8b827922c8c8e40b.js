4abe39e7fb7430bee364b2b2aefacef476770206
Viktoria Koval <kovalviktoria.hitech@gmail.com>
2021-09-13 10:15:47 +0300

    Updates

---
+++ precache-manifest.4af8f7a8cf9b593c8b827922c8c8e40b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d3c8ca79f0ae444b803ea9fea27821e",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "30895b20ce6be648ff48",
+    "url": "/goit-react-hw-02-feedback/static/js/2.cf8d5fe0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.cf8d5fe0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "123e51ce4da5d2213896",
+    "url": "/goit-react-hw-02-feedback/static/js/main.48b10a37.chunk.js"
+  },
+  {
+    "revision": "cb08e2ec47267fd4ee9f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.9cb7e66e.js"
+  }
+]);
